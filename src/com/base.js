2e0abe861b0a003208edc4e57e439ed882db103a
Oscar.chen <ChenXinqiao@chinasofti.com>
2018-12-14 16:10:16 +0800

    add img

--- src/com/base.js
+++ src/com/base.js
@@ -4,7 +4,18 @@ export default class Base {
 
     }
 
-    goTo(view, isBaseRouter) {
+    goTo (view, isBaseRouter) {
         isBaseRouter? location.hash = view: location.hash = 'tool/'+view;
+    }
+
+    leavePage (fun) {
+        if (typeof fun !== "function") throw new Error("baseController leavePage error: " + fun + "is not a function");
+
+        function _leave() {
+            fun();
+            window.removeEventListener("hashchange", _leave);
+        }
+
+        window.addEventListener("hashchange", _leave, false);
     }
 }
--- src/com/tool/index.js
+++ src/com/tool/index.js
@@ -5,6 +5,10 @@ import io from 'socket.io-client'
 import '../../lib/jquery.barrager.min'
 import Zhong from './Mr.Zhong.png'
 import bk from './lll/bk.jpg'
+import img2 from './lll/aerial-shot-bird-s-eye-view-daylight-1667003 (2).jpg'
+import img3 from './lll/android-wallpaper-back-view-boat-531474.jpg'
+import img4 from './lll/bird-s-eye-view-cold-daylight-1679718.jpg'
+import img5 from './lll/bridge-cars-daylight-1681392.jpg'
 
 class Tool extends Base{
     constructor() {
@@ -18,6 +22,7 @@ class Tool extends Base{
 
 
     run() {
+        const imgArray = [{key:1,path:bk}, {key:2,path:img2}, {key:3,path:img3}, {key:4,path:img4}, {key:5,path:img5}];
         const socket = io();
         socket.on('chat message', function(msg){
             const userInfo = msg.user;
@@ -32,6 +37,45 @@ class Tool extends Base{
             };
             $('#root').barrager(item);
         });
+        document.onkeydown = function(e) {
+            let key = $("#root")[0].dataset.key;
+            switch(e.keyCode) {
+                case 67: //c
+                    $.fn.barrager.removeAll();
+                    break;
+                case 37: //left
+                    let indexLeft = --key;
+                    if(indexLeft< 0) {
+                        indexLeft = imgArray.length - 1;
+                    }
+                    $('#root').css({
+                        'background-image': 'url('+imgArray[indexLeft].path+')'
+                    });
+                    $('#root')[0].dataset.key = indexLeft;
+                    break;
+                case 39: //right
+                    let indexRight = ++key;
+                    if(indexRight> imgArray.length - 1) {
+                        indexRight = 0;
+                    }
+                    $('#root').css({
+                        'background-image': 'url('+imgArray[indexRight].path+')'
+                    })
+                    $('#root')[0].dataset.key = indexRight;
+                    break;
+                default :
+
+
+            }
+        };
+
+        this.leavePage(function() {
+            document.onkeydown = null;
+        })
+
+
+
+
     }
 }
 
